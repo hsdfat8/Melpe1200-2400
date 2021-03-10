84db17a65d38aa69b31280297f8b6dc31a57beeb
hsdfat8 <phat2lechi@gmail.com>
2021-03-10 15:15:07 +0700

    first commit

---
+++ Makefile
@@ -0,0 +1,50 @@
+#   Make file for PairPhone
+#   Tested with gcc 4.4.3 under Ubuntu 10.04 (requires libsound2-dev)
+#   and MinGW (gcc 4.8.1, make 3.81) under Windows XP SP3
+
+
+# Debugging options  
+DEBUG = -O -DHEXDUMP
+
+#Full duplex:
+
+CC = gcc -Wall -g # for GNU's gcc compiler
+
+
+#   Compiler flags
+
+CFLAGS = $(DEBUG) $(PKOPTS) -Imeple $(CARGS) $(DUPLEX) $(CCFLAGS) $(DOMAIN)
+
+BINARIES = main
+
+PROGRAMS = $(BINARIES) $(SCRIPTS)
+
+DIRS =  melpe
+
+all:	$(PROGRAMS)
+
+SPKROBJS = main.o
+
+
+main: $(SPKROBJS) 
+	$(CC) $(SPKROBJS)  melpe/libmelpe.a $(LFLAGS) -o melpe
+
+
+
+#   Object file dependencies
+
+
+
+main.o: Makefile main.c
+
+
+#	Clean everything
+
+clean:
+	find . -name Makefile.bak -exec rm {} \;
+	del -f core *.out *.o *.bak $(PROGRAMS) *.shar *.exe *.a
+	@for I in $(DIRS); \
+	  do (cd $$I; echo "==>Entering directory `pwd`"; $(MAKE) $@ || exit 1); done
+	
+
+# DO NOT DELETE
---
+++ main.c
@@ -0,0 +1,97 @@
+///////////////////////////////////////////////
+//
+// **************************
+//
+// Project/Software name: X-Phone
+// Author: "Van Gegel" <gegelcopy@ukr.net>
+//
+// THIS IS A FREE SOFTWARE  AND FOR TEST ONLY!!!
+// Please do not use it in the case of life and death
+// This software is released under GNU LGPL:
+//
+// * LGPL 3.0 <http://www.gnu.org/licenses/lgpl.html>
+//
+// You’re free to copy, distribute and make commercial use
+// of this software under the following conditions:
+//
+// * You have to cite the author (and copyright owner): Van Gegel
+// * You have to provide a link to the author’s Homepage: <http://torfone.org>
+//
+///////////////////////////////////////////////
+
+//This is a main procedure of PairPhone testing software
+//one-thread implementation as a infinite loop contained procedures for:
+//-receiving baseband, demodulating, decrypting, decompressing, playing over earphones (RX)
+//-recording voice from mike, compressing, encrypting, modulating, sending baseband into line (TX)
+//-scan keyboard, processing. Suspending thread if no job (CTR)
+//---------------------------------------------------------------------------
+#ifdef _WIN32
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <stddef.h>
+#include <basetsd.h>
+#include <stdint.h>
+#include <windows.h>
+#include <time.h>
+#include <conio.h>
+#include <string.h>
+#include "memory.h"
+#include "math.h"
+
+#else //linux
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <time.h>
+#include "memory.h"
+#include "math.h"
+
+#include <sys/time.h>
+
+#endif
+
+#include "melpe/melpe.h"   //low-level alsa/wave audio
+static numfr;
+static short spin[748]; 
+static short spout[748]; 
+int main(int argc, char *argv[])
+{
+   int i = 0;
+   int j;
+  FILE *fin, *fout;
+
+  printf("-----------------------------------------------------\r\n");
+  printf("   		EDABK PairPhone VAD \r\n\n");
+  printf("-----------------------------------------------------\r\n");
+	printf("Running MELPe with: \nFile in \t:%s \nFile out\t:%s\n\n\n", argv[1], argv[2]);
+
+  fin = fopen(argv[1], "rb");
+  fout = fopen(argv[2], "wb");
+  short a = 0;
+  printf("-----------------------------------------------------\r\n");
+  printf("   		EDABK PairPhone MELPe \r\n\n");
+  printf("-----------------------------------------------------\r\n");
+    melpe_i();
+    numfr++;
+    unsigned char buf[12];
+    //  printf("\n\nFRAME %d\n",numf);
+    while(1)
+    {
+      j = fread(spin, sizeof(short), 540, fin);
+      
+      if (j < 540)
+      {
+          printf("Done MELPe!\n");
+          return 0;
+      }
+      melpe_a(buf,spin);
+      buf[9]&=0xFE;
+      printf("buf0 = %x     buf1 = %x\n",buf[0],buf[1]);
+      melpe_s(spout,buf);
+      
+      fwrite(spout, sizeof(short), 540, fout);  
+    }
+}
+//---------------------------------------------------------------------------
